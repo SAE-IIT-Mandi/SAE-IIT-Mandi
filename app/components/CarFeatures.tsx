5b737962616b46b637707f1022795f0ab846cabc
Vinamra-21 <135729551+Vinamra-21@users.noreply.github.com>
2024-08-10 19:03:01 +0530

    Add files via upload

---
+++ app/components/CarFeatures.tsx
@@ -0,0 +1,31 @@
+// components/CarFeatures.tsx
+import styles from './CarFeatures.module.css';
+import Image from 'next/image';
+
+const CarFeatures = () => {
+  return (
+    <section className={styles.featuresSection}>
+      <div className={styles.featureCard}>
+        <h2>Feature 1</h2>
+        <p>Details about the first feature.</p>
+      </div>
+      <div className={styles.featureCard}>
+        <h2>Feature 2</h2>
+        <p>Details about the second feature.</p>
+      </div>
+      <div className={styles.logoContainer}>
+        {<Image src="/images/sae.png" alt="SAE Logo" width={100} height={100} />}
+      </div>
+      <div className={styles.featureCard}>
+        <h2>Feature 3</h2>
+        <p>Details about the third feature.</p>
+      </div>
+      <div className={styles.featureCard}>
+        <h2>Feature 4</h2>
+        <p>Details about the fourth feature.</p>
+      </div>
+    </section>
+  );
+};
+
+export default CarFeatures;
---
+++ app/contactus/page.tsx
@@ -0,0 +1,15 @@
+// app/contact-us/page.js
+import Navbar from '../components/Navbar';
+
+export default function ContactUs() {
+  return (
+    <div>
+      <Navbar />
+      <main>
+        <h1>Contact Us</h1>
+        <p>Contact information and form.</p>
+      </main>
+    </div>
+  );
+}
+
