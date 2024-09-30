c9df047cd3f34b9c84c56a27365c51d6f6848c6f
Vinamra Garg <gargvinamra21@gmail.com>
2024-10-01 01:03:24 +0530

    commit

--- app/components/AboutUs.tsx
+++ app/components/AboutUs.tsx
@@ -14,9 +14,8 @@ We organize expert lectures on vehicle dynamics, and modern automotive technolog
 
                 <div className={styles.contact_info}>
                     <h2 className={styles.h2}>Get in Touch</h2>
-                    <p>If you have any questions, suggestions, or just want to reach out, feel free to contact us through the details provided below or use the contact form.</p>
+                    <p>If you have any questions, suggestions, or just want to reach out, feel free to contact us through the details provided below.</p>
                     <p>Email: <a href="mailto:sae@iitmandi.ac.in">sae@iitmandi.ac.in</a></p>
-                    <p>Phone: +91-1234567890</p>
                     <p>Address: SAE Club, IIT Mandi, Kamand, Himachal Pradesh, 175075 India</p>
                 </div>
 
