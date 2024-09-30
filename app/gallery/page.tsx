0a2d8cd62736bbcbc50c9f3acb378090ef338f69
Vinamra Garg <gargvinamra21@gmail.com>
2024-09-30 20:49:08 +0530

    commit

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,6 +1,7 @@
 import Navbar from '../components/Navbar';
 import styles from '../Home.module.css';
 import Gallerybox from '../components/Gallerybox';
+import Footer from '../components/Footer';
 export default function ContactUs() {
   return (
     <div>
@@ -8,27 +9,7 @@ export default function ContactUs() {
       <main>
         <Gallerybox />
       </main>
-      <footer className={styles.footer}>
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
-      <div className="footer-social-icons">
-        <a href="https://www.facebook.com/SAEiitmandi/" target="_blank" rel="noopener noreferrer" className="social-link facebook">
-          <i className="fab fa-facebook-f"></i>
-        </a>
-        <a href="https://github.com/" target="_blank" rel="noopener noreferrer" className="social-link github">
-          <i className="fab fa-github"></i>
-        </a>
-        <a href="https://www.instagram.com/sae.iitmandi/" target="_blank" rel="noopener noreferrer" className="social-link instagram">
-          <i className="fab fa-instagram"></i>
-        </a>
-        <a href="https://www.youtube.com/" target="_blank" rel="noopener noreferrer" className="social-link youtube">
-          <i className="fab fa-youtube"></i>
-        </a>
-        <a href="https://x.com/sae_iitmandi" target="_blank" rel="noopener noreferrer" className="social-link twitter">
-          <i className="fab fa-twitter"></i>
-        </a>
-      </div>
-      <p className="footer-text">Copyright &copy; 2024 All rights reserved by SAE, IIT Mandi</p>
-    </footer>
+      <Footer />
     </div>
   );
 }
