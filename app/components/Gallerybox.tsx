7b1c02fbc3d46d99630288b8504a99fb6fb4758f
Vinamra Garg <gargvinamra21@gmail.com>
2024-10-12 00:06:59 +0530

    gallery update

--- app/components/Gallerybox.tsx
+++ app/components/Gallerybox.tsx
@@ -1,34 +1,13 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import Image from "next/image";
-import styles from './Gallerybox.module.css';
+import styles from "./Gallerybox.module.css";
 
 const Gallerybox: React.FC = () => {
-  const [images, setImages] = useState<string[]>([
-    "/gallery/supra.jpg",
-    "/gallery/photo3.jpg",
-    "/gallery/photo4.jpg",
-    "/gallery/photo5.jpg",
-    "/gallery/photo6.jpg",
-    "/gallery/photo7.jpg",
-    "/gallery/page 1.jpg",
-    "/gallery/photo8.jpg",
-    "/gallery/page 3.jpg",
-    "/gallery/page 4.png",
-    "/gallery/sae1.jpg",
-    "/gallery/sae2.JPG",
-    "/gallery/sae3.jpg",
-    "/gallery/sae4.jpg",
-    "/gallery/sae5.jpg",
-    "/gallery/sae6.jpg",
-    "/gallery/sae7.png",
-    "/gallery/sae8.jpg",
-    "/gallery/sae9.jpg",
-  ]);
-
+  const galleryPaths = Array.from({ length: 84 }, (_, i) => `/gallery/${i + 1}.webp`); // Fixed the path format
+  const [images, setImages] = useState<string[]>(galleryPaths);
   const [selectedImage, setSelectedImage] = useState<string | null>(null);
 
-  // Function to shuffle images
   const shuffleImages = (array: string[]) => {
     const shuffled = [...array];
     for (let i = shuffled.length - 1; i > 0; i--) {
@@ -38,23 +17,20 @@ const Gallerybox: React.FC = () => {
     return shuffled;
   };
 
-  // Function to handle image click
   const handleImageClick = (src: string) => {
     setSelectedImage(src);
   };
 
-  // Function to close the overlay
   const handleOverlayClick = () => {
     setSelectedImage(null);
   };
 
-  // Shuffle images every 5 seconds with animation
   useEffect(() => {
     const interval = setInterval(() => {
       setImages((prevImages) => shuffleImages(prevImages));
-    }, 5000); // Adjust the interval time in milliseconds (5000ms = 5s)
+    }, 5000); 
 
-    return () => clearInterval(interval); // Clean up the interval on component unmount
+    return () => clearInterval(interval); 
   }, []);
 
   return (
@@ -64,13 +40,12 @@ const Gallerybox: React.FC = () => {
           <div
             className={styles.card}
             key={index}
-            onClick={() => handleImageClick(src)} // Set the clicked image
-            style={{ animationDelay: `${index * 0.1}s` }} // Stagger animation for each card
+            onClick={() => handleImageClick(src)} 
+            style={{ animationDelay: `${index * 0.1}s` }} 
           >
             <Image
               src={src}
               alt={`Gallery item ${index}`}
-              layout="fill"
               className={styles.image}
             />
           </div>
