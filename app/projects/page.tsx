0a64d5f5bc0660e908bfb2da6c61a6110b82e672
Vinamra Garg <gargvinamra21@gmail.com>
2024-12-18 16:44:54 +0530

    feature edit

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -4,14 +4,6 @@ import styles from './Projects.module.css';
 export default function ContactUs() {
   return (
       <main>
-      <nav className={styles.ovalNavbar}>
-        <div className={styles.navbarContent}>
-          <div><a href="/efficycleBrochure">Efficycle'24</a></div>
-          <div><a href="#">Formula Bharat</a></div>
-          <div><a href="#">Efficycle'19</a></div>
-          <div><a href="#">SAE Supra</a></div>
-        </div>
-      </nav>
         {/* <section className={styles.section}>
           <CarViewer />
         </section> */}
