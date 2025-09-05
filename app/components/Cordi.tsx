183a106d1bbc1e391434cbd6939953f67ff73cac
shivam <shivamsoni4335@gmail.com>
2025-09-05 13:06:11 +0530

    final(hopefully) changes by team sae'25

--- app/components/Cordi.tsx
+++ app/components/Cordi.tsx
@@ -5,22 +5,38 @@ import Image from "next/image";
 const Cordi: React.FC = () => {
   return (
     <section className={styles.cordiSection}>
-    <div className={styles.imageContainer}>
-      <Image src="/homepage/Badal.webp" alt="SAE Logo" width={280} height={280} />
+      <div className={styles.imageContainer}>
+        <Image
+          src="/team/sanskritiCordi.jpg"
+          alt="SAE Logo"
+          width={300}
+          height={0}
+          style={{ height: "auto" }}
+        />
       </div>
       <div className={styles.textContainer}>
         <h2>Coordinator's Note</h2>
         <p>
-        Welcome to IIT Mandi SAE Club's official webpage! As the Coordinator, I am honored to present our journey of innovation and engineering excellence. This platform highlights our projects that showcase the dedication and creativity of our team in transforming ideas into reality,and our valued sponsors, whose support fuels our aspirations. At SAE IIT Mandi, we are driven by a passion for pushing the boundaries of automotive engineering and fostering sustainable technological advancements.Through this website, we aim to connect with enthusiasts, professionals, and organizations to inspire collaboration and shape the future of engineering. Thank you for being a part of our journey!        </p>
+          Welcome to IIT Mandi SAE Club's official webpage! As the Coordinator,
+          I am honored to present our journey of innovation and engineering
+          excellence. This platform highlights our projects that showcase the
+          dedication and creativity of our team in transforming ideas into
+          reality,and our valued sponsors, whose support fuels our aspirations.
+          At SAE IIT Mandi, we are driven by a passion for pushing the
+          boundaries of automotive engineering and fostering sustainable
+          technological advancements.Through this website, we aim to connect
+          with enthusiasts, professionals, and organizations to inspire
+          collaboration and shape the future of engineering. Thank you for being
+          a part of our journey!{" "}
+        </p>
         <div className={styles.links}>
-          <Link href="/aboutUs" className={styles.link}>About Us</Link>
+          <Link href="/aboutUs" className={styles.link}>
+            About Us
+          </Link>
         </div>
       </div>
-      
     </section>
   );
 };
 
 export default Cordi;
-
-
